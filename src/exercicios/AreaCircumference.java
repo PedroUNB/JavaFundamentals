ff7d4a5c424fac3487820b02987cd9572c58d119
Pedro <pedro.oliveira.unb@gmail.com>
2020-12-29 15:00:41 -0300

    First Commit

---
+++ src/exercicios/AreaCircumference.java
@@ -0,0 +1,12 @@
+package exercicios;
+
+public class AreaCircumference {
+  public static void main(String[] args) {
+    int raio = 4;
+    final double PI = 3.14159;
+
+    double area = PI * raio * raio;
+
+    System.out.println("Área = " + area);
+  }
+}
---
+++ src/exercicios/Tipagem.java
@@ -0,0 +1,20 @@
+package exercicios;
+
+public class Tipagem {
+  public static void main(String[] args) {
+    // tipos inteiros
+    byte a = 127;
+    short b = 32767;
+    int c = 2_134_845_223;
+    long d = 3_134_845_223L;
+    // tipos reais = bits
+    double e = 2_991_797_103.01;
+    float f = 11_445.44F;
+    // Tipos primitivos
+    char g = 'a';
+    boolean h = true;
+
+    // conversão de tipos
+
+  }
+}
